e64e367dfa6d1c593a5fdb04127d43e00eaee134
jhony2022 <thecapoflash@hotmail.com>
2022-11-15 02:52:08 -0300

    agregando casi todo

--- codigo.js
+++ codigo.js
@@ -23,6 +23,10 @@ let carrito = []
 let juegosDivPc = document.getElementById("losJuegosPc");
 let juegosDivPs = document.getElementById("losJuegosPs");
 
+
+
+//FUNCION CREANDO LAS TARGETAS DE MIS JUEGOS Y AGREGANDO EVENTO AL BOTON DE COMPRA.
+
 function editandoHtml(){
     for (const juego of juegosPc){
         juegosDivPc.innerHTML += `
@@ -31,7 +35,7 @@ function editandoHtml(){
             <div class="juegos_texto">
                 <h3>${juego.nombreTapa}</h3>
                 <p>${juego.precio} $</p>
-                <a id="btn${juego.id}">Comprar</a>
+                <a  id="btn${juego.id}">Comprar</a>
             </div>
         </div>
         `;
@@ -43,61 +47,171 @@ function editandoHtml(){
             <div class="juegos_texto">
                 <h3>${juego.nombreTapa}</h3>
                 <p>${juego.precio} $</p>
-                <a id="btn${juego.id}">Comprar</a>
+                <a  id="btn${juego.id}">Comprar</a>
             </div>
         </div>
         `;
     }
 
 
-    //EVENTOS
+    //EVENTOS PARA AGREGAR EL JUEGO AL CARRITO
 
-    juegosPc.forEach(juego => {
-        document.getElementById(`btn${juego.id}`).addEventListener("click", function(){
-            agregarAlCarrito(juego);
+        juegosPc.forEach(juego => {
+            document.getElementById(`btn${juego.id}`).addEventListener("click", function(){
+                agregarAlCarrito(juego.id);
+            })
         })
-    })
-    juegosPs.forEach(juego => {
-        document.getElementById(`btn${juego.id}`).addEventListener("click", function(){
-            agregarAlCarrito(juego);
+        juegosPs.forEach(juego => {
+            document.getElementById(`btn${juego.id}`).addEventListener("click", function(){
+                agregarAlCarrito(juego.id);
+            })
         })
-    })
     
 }
+
 editandoHtml();
 
-function agregarAlCarrito(juegoComprado){
-    carrito.push(juegoComprado);
-    Swal.fire({
-        position: 'top-end',
-        color: 'aqua',
-        background: 'rgb(48, 48, 48)',
-        imageAlt: juegoComprado.nombre,
-        title: juegoComprado.nombre,
-        imageUrl: juegoComprado.imagen,
-        imageWidth: 150,
-        imageHeight: 100,
-        text: 'Agregado al carrito!',
-        showConfirmButton: false,
-        timer: 850
+
+
+//FUNCION PARA AGREGAR LOS JUEGOS AL CARRITO.
+
+
+function agregarAlCarrito(id){
+
+    const juegoExiste = carrito.some(juego => juego.id === id)
+    if (juegoExiste){
+        carrito.map(juego => {
+            if (juego.id === id){
+                juego.cantidad++
+            }
         })
+        
+    }
+    else {
+        const item  = juegosPc.find((juego) => juego.id === id)
+        const item2 = juegosPs.find((juego) => juego.id === id)
+        carrito.push(item || item2);
+    }
+
+
+
+    const juegoNombreId = carrito.find((juego) => juego.id === id)
+
+            Swal.fire({
+                position: 'top-end',
+                color: 'aqua',
+                background: 'rgb(48, 48, 48)',
+                imageAlt: juegoNombreId.nombre,
+                title: juegoNombreId.nombre,
+                icon: 'success',
+                imageWidth: 150,
+                imageHeight: 100,
+                text: '¡ Agregado al carrito !',
+                showConfirmButton: false,
+                timer: 750
+            })
     console.table(carrito);
-    let tableBody = document.getElementById("tableBody")
-    tableBody.innerHTML += `
-    <tr>
-        <td>${juegoComprado.nombre}</td>
-        <td>${juegoComprado.id}</td>
-        <td>${juegoComprado.precio} $</td>
-    </tr>
-    `;
-    let sumaTotal = document.getElementById("sumaTotal");
-    const total = carrito.reduce((acc, juego)=> acc + juego.precio,0 );
-    sumaTotal.innerHTML = `Total a pagar :  <span class="spanTotal">${total} $</span>`;
+    
+    dibujarCarrito();
+    
     vaciarDevolucion();
+    
+    guardarStorage();
+
 }
+
+//VACIANDO EL CARRITO
+let vaciarTodo = document.getElementById("vaciarTodo");
+
+vaciarTodo.addEventListener("click", () => {
+    if(carrito.length === 0){
+        Swal.fire({
+            icon: 'error',
+            color: 'rgb(252, 110, 110)',
+            background: 'rgb(48, 48, 48)',
+            title: `¡ Su carrito esta vacio !`,
+            showConfirmButton: false,
+            timer: 750
+        })
+    }
+    else{
+        carrito.length = 0;
+        dibujarCarrito();
+        guardarStorage();
+    }
+})
+
+
+//FUNCION PARA PINTAR LOS JUEGOS COMPRADOS EN LA TABLA(CARRITO).
+
+const dibujarCarrito = () => {
+    let tableBody = document.getElementById("tableBody")
+    tableBody.innerHTML = ` `;
+    carrito.forEach((juego) => {
+        tableBody.innerHTML += `
+            <tr>
+                <td><span class="tablaNombre">${juego.nombre}</span></td>
+                <td>${juego.cantidad}</td>
+                <td>${juego.id}</td>
+                <td><span class="tablaPrecio">${juego.precio} $</span></td>
+                <td><button class="eliminarCarrito" onclick="eliminarDelCarrito(${juego.id})"><img class="eliminarCarritoImg" src="./assets/iconos/contenedor-de-basura.png" alt="contenedorBasura" width="15px"></button></td>
+            </tr>
+            `;
+    })
+    
+    
+    let sumaTotal = document.getElementById("sumaTotal");
+        const total = carrito.reduce((acc, juego)=> acc + juego.cantidad * juego.precio,0 );
+        sumaTotal.innerHTML = `Total a pagar :  <span class="spanTotal">${total} $</span>`;
+        
+        if(carrito.length === 0){
+            sumaTotal.innerHTML = `Total a pagar : `;
+        }
+
+}
+
+
+//FUNCION PARA ELIMINAR LOS JUEGOS DEL CARRITO UNO POR UNO.
+
+function eliminarDelCarrito(id){
+    let juegoId = id;
+    const juegoExistente = carrito.some(juego => juego.id === juegoId)
+    if (juegoExistente){
+        carrito.map(juego => {
+            if (juego.id === id){
+                if (juego.cantidad > 1){
+                    juego.cantidad--
+                    
+                }
+                else if(juego.cantidad === 1){
+                    carrito = carrito.filter((juego) => juego.id !== juegoId);
+                }
+                
+            }
+            
+            
+        })
+        
+    }
+    else {
+        carrito = carrito.filter((juego) => juego.id !== juegoId);
+        
+    }
+
+    guardarStorage();
+
+    dibujarCarrito();
+    
+}
+
+
 
 let botonFinal = document.getElementById("botonFinalizar");
 let pDevolucion = document.getElementById("parrafoDevolucion");
+let tablaHead = document.getElementById("tableHeadDev");
+let tablaBody = document.getElementById("tableBodyDev");
+let totalFinal = document.getElementById("totalFinal");
+
 
 botonFinal.addEventListener("click", function(){
     laDevolucion();
@@ -105,32 +219,106 @@ botonFinal.addEventListener("click", function(){
 
 
 
+//FUNCION PARA MOSTRAR LOS RESULTADOS DE LA COMPRA AL FINALIZARLA.
 
 function laDevolucion(){
-    let carritoJuegos = carrito.map((juego) => juego.nombre);
-    const total = carrito.reduce((acc, juego)=> acc + juego.precio,0 );
-    pDevolucion.innerHTML = `COMPRA FiNALIZADA! <br> <br>
-    Juegos comprados: <br> <span class="juegosComprados">${carritoJuegos.join(" , ")}</span><br> <br>
-    Total a pagar: <span class="spanTotal2">${total} $</span> <br> <br>
-    Gracias por su compra!`;
-    tableBody.innerHTML = `
-    `;
-    sumaTotal.innerHTML = `Total a pagar : `;
-    carrito.length = 0
 
+    
+    if(carrito.length === 0){
+        Swal.fire({
+            icon: 'error',
+            color: 'rgb(252, 110, 110)',
+            background: 'rgb(48, 48, 48)',
+            title: `¡ Su carrito esta vacio !`,
+            showConfirmButton: false,
+            timer: 750
+        })
+        vaciarDevolucion();
+        guardarStorage();
+    }
+    else{
+        let carritoJuegos = carrito.map((juego) => `${juego.nombre} (${juego.cantidad})`);
+    const total = carrito.reduce((acc, juego)=> acc + juego.cantidad * juego.precio,0 );
+    pDevolucion.innerHTML = `¡ COMPRA FiNALIZADA! <br> <br>
+    Juegos comprados: 
+    `;
+    tablaHead.innerHTML = `
+    <tr>
+        <th>Nombre</th>
+        <th>Unidades</th>
+        <th>Suma</th>
+    </tr>
+    `;
+    for(const juego of carrito){
+        tablaBody.innerHTML += `
+        <tr>
+            <td>${juego.nombre}</td>
+            <td>${juego.cantidad}</td>
+            <td>${juego.precio * juego.cantidad} $</td>
+        </tr>
+    `;
+    }
+    totalFinal.innerHTML = `
+    <p>TOTAL A PAGAR:  <span class="spanTotalFinal"> ${total} $</span></p>
+    <p> ¡ Gracias por su compra ! </p>
+    `;
+    carrito.length = 0;
+    dibujarCarrito();
+    guardarStorage();
+    const cantidadJuegos = juegosPc.map((juego) => juego.cantidad = 1) && juegosPs.map((juego) => juego.cantidad = 1);
+    }
+    
 }
 
 function vaciarDevolucion(){
     pDevolucion.innerHTML = ` `;
+    tablaHead.innerHTML = ` `;
+    tablaBody.innerHTML = ` `;
+    totalFinal.innerHTML = ` `;
 }
 
 
+function guardarStorage(){
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        carrito = JSON.parse(localStorage.getItem("carrito")) || []
+        dibujarCarrito();
+    })
 
 
 
+// PONIENDO MODO DARK Y LIGHT
 
+let botonDarkLight = document.getElementById(`darkLightBoton`)
+modo = localStorage.getItem("modo");
 
+if(modo !== null){
+    document.body.className = modo
+    if(modo === "darkMode"){
+        botonDarkLight.innerText="Light-Mode"
+    }
+    else{
+        botonDarkLight.innerText="Dark-Mode"
+    }
+}
+else{
+    modo= "darkMode";
+}
 
+botonDarkLight.addEventListener("click", () => {
+    if(modo == "darkMode"){
+        document.body.className="lightMode";
+        botonDarkLight.innerText="Dark-Mode";
+        modo="lightMode";
+    }else{
+        document.body.classList.remove("lightMode")
+        botonDarkLight.innerText="Light-Mode"
+        modo="darkMode";
+    }
+    localStorage.setItem("modo", modo)
+})
 
 
 
--- juegos.js
+++ juegos.js
@@ -5,30 +5,35 @@
 const juegosPc = [
     {nombre: `Fifa23`, 
     nombreTapa: `Fifa 23`,
+    cantidad: 1,
     precio: 150,
     id: 1, 
     imagen: "./assets/img/juegos/fifa23.jpg"},
 
     {nombre: `Gta5`, 
     nombreTapa: `Gta 5`, 
+    cantidad: 1,
     precio: 150, 
     id: 2, 
     imagen: "./assets/img/juegos/gtav.jpg"},
 
     {nombre: `Call of duty 4`, 
     nombreTapa: `COD 4`,
+    cantidad: 1,
     precio: 100, 
     id: 3, 
     imagen: "./assets/img/juegos/codbo4.jpg"},
 
     {nombre: `Resident evil 6`, 
     nombreTapa: `RE 6`,
+    cantidad: 1,
     precio: 80, 
     id: 4,
     imagen: "./assets/img/juegos/residentevil6.jpg"},
 
     {nombre: `Counter strike go`,
     nombreTapa: `CS GO`,
+    cantidad: 1,
     precio: 120, 
     id: 5,
     imagen: "./assets/img/juegos/csgo.jpg"}
@@ -39,30 +44,35 @@ const juegosPc = [
 const juegosPs = [
     {nombre: `The last of us`,
     nombreTapa: `TLOU`,
+    cantidad: 1,
     precio: 200, 
     id: 6,
     imagen: "./assets/img/juegos/thelastofus.jpg"},
 
     {nombre: `God of war`, 
     nombreTapa: `GOW`,
+    cantidad: 1,
     precio: 120, 
     id: 7,
     imagen: "./assets/img/juegos/godofwar.jpg"},
 
     {nombre: `Ufc 4`, 
     nombreTapa: `UFC 4`,
+    cantidad: 1,
     precio: 150, 
     id: 8,
     imagen: "./assets/img/juegos/ufc4.jpg"},
 
     {nombre: `Uncharted 4`, 
     nombreTapa: `Unch 4`,
+    cantidad: 1,
     precio: 120, 
     id: 9,
     imagen: "./assets/img/juegos/uncharted4.png"},
 
     {nombre: `Spiderman`, 
     nombreTapa: `Spiderman`,
+    cantidad: 1,
     precio: 100, id: 10,
     imagen: "./assets/img/juegos/spidermanps4.jpg"}
 ];
